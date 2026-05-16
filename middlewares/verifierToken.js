97429c8fb89d005e43cddd36f8094e2889244f09
Ikram El Gliti <ikramel573ff@gmail.com>
2026-05-16 22:38:30 +0100

    fix: verifierToken correct order

--- middlewares/verifierToken.js
+++ middlewares/verifierToken.js
@@ -1,17 +1,8 @@
 const jwt = require('jsonwebtoken');
-const pool=require('../config/db');
+const pool = require('../config/db');
 
 async function verifierToken(req, res, next) {
-    const decoded = jwt.verify(token, secret);
-    req.userId = decoded.userId;
-    const result = await pool.query(
-    'SELECT role, nom FROM users WHERE id = $1',
-    [decoded.userId]
-    );
-    req.userRole = result.rows[0].role;
-    req.userName = result.rows[0].nom;
     const authHeader = req.headers.authorization;
-    console.log('authHeader:', authHeader);
     if (!authHeader) {
         return res.status(401).json({ error: 'Token manquant' });
     }
@@ -21,9 +12,11 @@ async function verifierToken(req, res, next) {
         const decoded = jwt.verify(token, secret);
         req.userId = decoded.userId;
         const result = await pool.query(
-        'SELECT role FROM users WHERE id = $1',
-        [decoded.userId]);
+            'SELECT role, nom FROM users WHERE id = $1',
+            [decoded.userId]
+        );
         req.userRole = result.rows[0].role;
+        req.userName = result.rows[0].nom;
         next();
     } catch (err) {
         return res.status(401).json({ error: 'Token invalide' });
